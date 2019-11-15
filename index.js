87ddd8c915614d5a3c689e3659e8ccc7830aea98
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-11-15 09:59:35 +0100

    register endpoint works

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server.js');
-
+require('dotenv').config();
 const PORT = process.env.PORT || 3300;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
